Fixed numerical stability issue in TubularPattern.wrapToWidth(). Some other reorganization.

KinematicTree.py
@@ -11,6 +11,9 @@ from dubinsPath import *
 import scipy
 from scipy.optimize import NonlinearConstraint, minimize
 import queue
+import tubularOrigami
+from tubularOrigami import *
+
 
 class KinematicTree:
     """
@@ -126,7 +129,6 @@ class KinematicTree:
                                    PoseW1, parent.pathIndex())
         # don't add in W1 yet because don't want to update bounding sphere?
     """            
-        
     
     
     def addToPlot(self, ax, xColor='r', yColor='b', zColor='g', 

tubularOrigami.py
@@ -59,10 +59,7 @@ TODO:
     Make variable naming consistent:
         Matrices/arrays of >1 axis: Capitalize
         Vectors/1-axis arrays: plural, don't capitalize
-        Vcalars: singular, don't capitalize
-    
-    Review all uses of %, np.mod, and math.remainder to make sure the correct
-    one is being used for the situation
+        scalars: singular, don't capitalize
     
     Figure out how to append revolute joints (mountain folds at the bases)
     and twist fittings (valley folds at the bases)
@@ -80,6 +77,7 @@ import geometryHelpers
 from geometryHelpers import rollToMatch, unsignedAngles, signedAngle, \
     signedAngles2D, signedAngles3D, lerpN, unit, signedAngle2D, unsignedAngle
 from spatialmath import SO3, SE3
+
 
 class TubularPattern():
     def __init__(self, numSides, r, proximalMarker=[0, 0]):
@@ -151,6 +149,13 @@ class TubularPattern():
         self.addEdges(EdgesToAdd, newFalses, deDuplicate)
 
     def wrapToWidth(self):
+        # For numerical stability, need to ensure that x coordinates 
+        # within EPSILON of 0 or width are treated as 0 or width
+        basicallyZero = abs(self.Vertices[:, 0]) < self.EPSILON
+        self.Vertices[basicallyZero, 0] = 0
+        basicallyWidth = abs(self.Vertices[:, 0] - self.width) < self.EPSILON
+        self.Vertices[basicallyWidth, 0] = self.width
+        
         self.Vertices[:, 0] %= self.width
         self.proximalMarker[0, 0] %= self.width
         self.distalMarker[0, 0] %= self.width
@@ -352,7 +357,7 @@ class TubularPattern():
 
         return doc
 
-class Tube(TubularPattern):
+class TubeFittingPattern(TubularPattern):
     def __init__(self, numSides, r, height, proximalMarker=[0, 0]):
         super().__init__(numSides, r, proximalMarker)
         self.patternHeight = height
@@ -894,7 +899,7 @@ class PrismaticJointPattern(TubularPattern):
         self.distalMarker = self.proximalMarker + [0, self.patternHeight]
         self.wrapToWidth()  
            
-class ElbowFitting(TubularPattern):
+class ElbowFittingPattern(TubularPattern):
     def __init__(self, numSides, r, bendingAngle, rotationalAxisAngle,
                  proximalMarker=[0,0]):
         super().__init__(numSides, r, proximalMarker)
@@ -1013,7 +1018,7 @@ class ElbowFitting(TubularPattern):
         self.distalMarker = self.proximalMarker + [0, self.patternHeight]
         self.wrapToWidth()
 
-class Twist(TubularPattern):
+class TwistFittingPattern(TubularPattern):
     def __init__(self, numSides, r, twistAngle, moduleHeight,
                  proximalMarker=[0, 0]):
         super().__init__(numSides, r, proximalMarker)
@@ -1054,72 +1059,3 @@ class Twist(TubularPattern):
         self.distalMarker = self.proximalMarker + \
             np.array([referenceXshift, self.patternHeight])
         self.wrapToWidth()
-
-
-
-
-""" Testing """
-import os
-import errno
-# https://stackoverflow.com/questions/32123394/workflow-to-create-a-folder-if-it-doesnt-exist-already
-def make_sure_path_exists(path):
-    try:
-        os.makedirs(path)
-    except OSError as exception:
-        if exception.errno != errno.EEXIST:
-            raise
-
-r = 1
-numSides = 6
-folder = "./examplePatterns/"
-make_sure_path_exists(folder)
-subfolder = folder+"r"+str(r)+"n"+str(numSides)+"/"
-make_sure_path_exists(subfolder)
-
-composed = TubularPattern(numSides, r)
-tube = Tube(numSides, r, 2)
-#tube.plotRawGraph(saveas=subfolder+"tube_raw_graph", directed=True)
-tube.makeDXF(saveas=subfolder+"tube", show=False)
-
-#tube.plotRawGraph(directed=True)
-composed.append(Tube(numSides, r, 3))
-twist = Twist(numSides, r, 0.45*np.pi, 1)
-#twist.plotRawGraph(saveas=subfolder+"twist_raw_graph", directed=True)
-twist.makeDXF(saveas=subfolder+"twist", show=False)
-composed.append(Twist(numSides, r, 0.9*np.pi, 1))
-composed.append(Tube(numSides, r, 3))
-composed.makeDXF(saveas=subfolder+"tube_twist_tube", show=False)
-#composed.plotRawGraph(saveas=subfolder+"tube_twist_tube_raw_graph", directed=True)
-
-doubleTwist = TubularPattern(numSides, r)
-doubleTwist.append(Twist(numSides, r, 0.9*np.pi, 1))
-doubleTwist.append(Twist(numSides, r, 0.3*np.pi, 1))
-doubleTwist.makeDXF(saveas=subfolder+"twist_twist", show=False)
-#doubleTwist.plotRawGraph(saveas=subfolder+"twist_twist_raw_graph", directed=True)
-
-elbow = ElbowFitting(numSides, r, -np.pi/4, np.pi/3)
-#elbow.plotRawGraph(saveas=subfolder+"elbow_raw_graph", directed=True)
-elbow.makeDXF(saveas=subfolder+"elbow", show=False)
-composed.append(elbow)
-composed.makeDXF(saveas=subfolder+"tube_twist_tube_elbow", show=False)
-
-prismatic = PrismaticJointPattern(numSides, r, 1, 3, np.pi/3)
-#prismatic.plotRawGraph(saveas=subfolder+"prismatic_raw_graph", directed=True)
-prismatic.makeDXF(saveas=subfolder+"prismatic", show=False)
-composed.append(prismatic)
-composed.makeDXF(saveas=subfolder+"tube_twist_tube_elbow_prismatic", show=False)
-composed.append(Tube(numSides, r, 1))
-composed.makeDXF(saveas=subfolder+"tube_twist_tube_elbow_prismatic_tube", show=False)
-
-revolute = RevoluteJointPattern(numSides, r, totalBendingAngle=np.pi, numSinkLayers=1)
-#revolute.plotRawGraph(saveas=subfolder+"revolute_raw_graph", directed=True)
-revolute.makeDXF(saveas=subfolder+"revolute_no_sink", show=False)
-
-
-revolute = RevoluteJointPattern(numSides, r, totalBendingAngle=np.pi, numSinkLayers=3)
-#revolute.plotRawGraph(saveas=subfolder+"revolute_raw_graph", directed=True)
-revolute.makeDXF(saveas=subfolder+"revolute", show=False)
-composed.append(revolute)
-composed.append(Tube(numSides, r, 0.5))
-composed.makeDXF(saveas=subfolder+"tube_twist_tube_elbow_prismatic_tube_revolute_tube",
-                 show=True)