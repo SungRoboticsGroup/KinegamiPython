Fixed numerical stability bug in wrapAngle (which had been causing a path to have turn angle essentially 2pi when that should have wrapped to 0)

Joint.py
@@ -26,7 +26,7 @@ class Joint(ABC):
         self.Pose = Pose
         self.neutralLength = neutralLength
         self.state = 0
-        self.transformStateTo(initialState)
+        self.TransformStateTo(initialState)
     
     @abstractmethod #0 for xhat, 2 for zhat
     def pathIndex(self) -> int:
@@ -48,19 +48,19 @@ class Joint(ABC):
     def boundingBall(self) -> Ball:
         pass
     
-    def proximalPose(self) -> SE3:
+    def ProximalPose(self) -> SE3:
         return SE3.Trans(-(self.neutralLength/2) * self.pathDirection()) @ self.Pose
     
-    def neutralDistalPose(self) -> SE3:
+    def NeutralDistalPose(self) -> SE3:
         return SE3.Trans((self.neutralLength/2) * self.pathDirection()) @ self.Pose
     
-    def stateTransformationFromNeutral(self) -> SE3:
+    def StateTransformationFromNeutral(self) -> SE3:
         return self.stateChangeTransformation(self.state)
     
-    def distalPose(self) -> SE3:
-        return self.stateTransformationFromNeutral() @ self.neutralDistalPose()        
+    def DistalPose(self) -> SE3:
+        return self.StateTransformationFromNeutral() @ self.NeutralDistalPose()        
     
-    def transformStateTo(self, state : float) -> SE3:
+    def TransformStateTo(self, state : float) -> SE3:
         minState, maxState = self.stateRange()
         assert(minState <= state and state <= maxState)
         stateChange = state - self.state
@@ -76,20 +76,20 @@ class Joint(ABC):
         return np.hstack((np.roll(np.arange(3), -self.pathIndex()),[3]))
     
     # Pose with axes cycled so that the first axis direction is pathDirection
-    def dubinsFrame(self) -> SE3:
+    def DubinsFrame(self) -> SE3:
         return SE3(self.Pose.A[:,self.dubinsColumnOrder()])
     
-    def proximalDubinsFrame(self) -> SE3():
-        return SE3(self.proximalPose().A[:,self.dubinsColumnOrder()])
+    def ProximalDubinsFrame(self) -> SE3():
+        return SE3(self.ProximalPose().A[:,self.dubinsColumnOrder()])
     
-    def distalDubinsFrame(self) -> SE3():
-        return SE3(self.distalPose().A[:,self.dubinsColumnOrder()])
+    def DistalDubinsFrame(self) -> SE3():
+        return SE3(self.DistalPose().A[:,self.dubinsColumnOrder()])
     
     def proximalPosition(self) -> np.ndarray:
-        return self.proximalPose().t
+        return self.ProximalPose().t
     
     def distalPosition(self) -> np.ndarray:
-        return self.distalPose().t
+        return self.DistalPose().t
     
     def transformPoseBy(self, Transformation : SE3):
         self.Pose = Transformation @ self.Pose
@@ -111,7 +111,7 @@ class Joint(ABC):
     
     def addToPlot(self, ax, xColor=xColorDefault, yColor=yColorDefault, zColor=zColorDefault, 
              proximalColor='c', centerColor='m', distalColor='y',
-             sphereColor='black', showSphere=False, surfaceColor='m',
+             sphereColor=sphereColorDefault, showSphere=False, surfaceColor='m',
              surfaceOpacity=surfaceOpacityDefault, showSurface=True, showAxis=False, 
              axisScale=10, showPoses=True):
         if showAxis:
@@ -123,7 +123,7 @@ class Joint(ABC):
         if showSphere:
             self.boundingBall().addToPlot(ax, color=sphereColor, alpha=0.05)
         if showPoses:
-            Poses = np.array([self.proximalPose(), self.distalPose(), self.Pose])
+            Poses = np.array([self.ProximalPose(), self.DistalPose(), self.Pose])
             oColors = np.array([proximalColor, distalColor, centerColor])
             plotHandles = addPosesToPlot(Poses, ax, self.r, 
                                          xColor, yColor, zColor, oColors)
@@ -134,7 +134,7 @@ class Joint(ABC):
     
     def show(self, xColor=xColorDefault, yColor=yColorDefault, zColor=zColorDefault, 
              proximalColor='c', centerColor='m', distalColor='y',
-             sphereColor='black', showSphere=False, surfaceColor='m',
+             sphereColor=sphereColorDefault, showSphere=False, surfaceColor='m',
              surfaceOpacity=surfaceOpacityDefault, showSurface=True, showAxis=False,
              axisScale=jointAxisScaleDefault, showPoses=True, block=blockDefault):
         ax = plt.figure().add_subplot(projection='3d')
@@ -190,7 +190,7 @@ class RevoluteJoint(OrigamiJoint):
     
     def addToPlot(self, ax, xColor=xColorDefault, yColor=yColorDefault, zColor=zColorDefault, 
              proximalColor='c', centerColor='m', distalColor='y',
-             sphereColor='black', showSphere=False, surfaceColor='m',
+             sphereColor=sphereColorDefault, showSphere=False, surfaceColor='m',
              surfaceOpacity=surfaceOpacityDefault, showSurface=True, showAxis=True,
              axisScale=10, showPoses=True):
         plotHandles = super().addToPlot(ax, xColor, yColor, zColor, proximalColor,
@@ -207,7 +207,7 @@ class RevoluteJoint(OrigamiJoint):
             angle = np.linspace(0, 2*np.pi, radialCount) + np.pi/self.numSides
             u = self.r * np.cos(angle)
             v = self.r * np.sin(angle)
-            ProximalPose = self.proximalPose()
+            ProximalPose = self.ProximalPose()
             uhatProximal = ProximalPose.R[:,1]
             vhatProximal = ProximalPose.R[:,2]
             ProximalBase = ProximalPose.t + u.reshape(-1,1) @ uhatProximal.reshape(1,3) + v.reshape(-1,1) @ vhatProximal.reshape(1,3)
@@ -220,7 +220,7 @@ class RevoluteJoint(OrigamiJoint):
                 tri.set_alpha(surfaceOpacity)
                 ax.add_collection3d(tri)
             
-            DistalPose = self.distalPose()
+            DistalPose = self.DistalPose()
             uhatDistal = DistalPose.R[:,1]
             vhatDistal = DistalPose.R[:,2]
             DistalBase = DistalPose.t + u.reshape(-1,1) @ uhatDistal.reshape(1,3) + v.reshape(-1,1) @ vhatDistal.reshape(1,3)
@@ -269,11 +269,11 @@ class PrismaticJoint(OrigamiJoint):
         return Ball(self.center(), self.boundingRadius())
     
     def boundingCylinder(self) -> Cylinder:
-        return Cylinder(self.r, self.proximalPose().t, self.pathDirection(), self.length())
+        return Cylinder(self.r, self.ProximalPose().t, self.pathDirection(), self.length())
     
     def addToPlot(self, ax, xColor=xColorDefault, yColor=yColorDefault, zColor=zColorDefault, 
              proximalColor='c', centerColor='m', distalColor='y',
-             sphereColor='black', showSphere=False, surfaceColor='m',
+             sphereColor=sphereColorDefault, showSphere=False, surfaceColor='m',
              surfaceOpacity=surfaceOpacityDefault, showSurface=True, showAxis=True, 
              axisScale=10, showPoses=True):
         plotHandles = super().addToPlot(ax, xColor, yColor, zColor, proximalColor,
@@ -289,6 +289,7 @@ class PrismaticJoint(OrigamiJoint):
 class Waypoint(OrigamiJoint):
     # path direction through a waypoint defaults to zhat
     def __init__(self, numSides : int, r : float, Pose : SE3, pathIndex : int = 2):
+        assert(pathIndex in [0,1,2])
         super().__init__(numSides, r, 0, Pose)
         self.pidx = pathIndex
         self.pattern = TubularPattern(numSides, r)
@@ -307,6 +308,28 @@ class Waypoint(OrigamiJoint):
     
     def boundingBall(self) -> Ball:
         return Ball(self.Pose.t, self.boundingRadius())
+    
+    def addToPlot(self, ax, xColor=xColorDefault, yColor=yColorDefault, zColor=zColorDefault, 
+             proximalColor='c', centerColor='m', distalColor='y',
+             sphereColor=sphereColorDefault, showSphere=False, surfaceColor='m',
+             surfaceOpacity=surfaceOpacityDefault, showSurface=True, showAxis=False, 
+             axisScale=10, showPoses=True):
+        if showAxis:
+            zhat = self.Pose.R[:,2]
+            JointAxis = np.array([self.Pose.t - axisScale*self.r*zhat,
+                                  self.Pose.t + axisScale*self.r*zhat])
+            ax.plot(JointAxis[:,0], JointAxis[:,1], JointAxis[:,2], 
+                    linestyle='--', color='silver')
+        if showSphere:
+            self.boundingBall().addToPlot(ax, color=sphereColor, alpha=0.05)
+        if showPoses:
+            Poses = np.array([self.Pose])
+            oColors = np.array([centerColor])
+            plotHandles = addPosesToPlot(Poses, ax, self.r, 
+                                         xColor, yColor, zColor, oColors)
+        else:
+            plotHandles = None
+        return plotHandles
 
 class Fingertip(OrigamiJoint):
     def __init__(self, numSides : int, r : float, Pose : SE3, length : float, 
@@ -332,7 +355,7 @@ class Fingertip(OrigamiJoint):
     
     def addToPlot(self, ax, xColor=xColorDefault, yColor=yColorDefault, zColor=zColorDefault, 
              proximalColor='c', centerColor='m', distalColor='y',
-             sphereColor='black', showSphere=False, surfaceColor='m',
+             sphereColor=sphereColorDefault, showSphere=False, surfaceColor='m',
              surfaceOpacity=surfaceOpacityDefault, showSurface=True, showAxis=False,
              axisScale=10, showPoses=True):
         plotHandles = super().addToPlot(ax, xColor, yColor, zColor, proximalColor,
@@ -350,11 +373,11 @@ class Fingertip(OrigamiJoint):
             tipSegmentIndex, uhatIndex, vhatIndex = 0,0,1
             
             if self.forward:
-                DistalPose = self.distalPose()
+                DistalPose = self.DistalPose()
                 TipSegment = np.array([DistalPose.t - scale * DistalPose.R[:,tipSegmentIndex],
                                           DistalPose.t + scale * DistalPose.R[:,tipSegmentIndex]])
                 
-                ProximalPose = self.proximalPose()
+                ProximalPose = self.ProximalPose()
                 uhatProximal = ProximalPose.R[:,uhatIndex]
                 vhatProximal = ProximalPose.R[:,vhatIndex]
                 ProximalBase = ProximalPose.t + u.reshape(-1,1) @ uhatProximal.reshape(1,3) + v.reshape(-1,1) @ vhatProximal.reshape(1,3)
@@ -366,11 +389,11 @@ class Fingertip(OrigamiJoint):
                     tri.set_alpha(surfaceOpacity)
                     ax.add_collection3d(tri)
             else:
-                ProximalPose = self.proximalPose()
+                ProximalPose = self.ProximalPose()
                 TipSegment = np.array([ProximalPose.t - scale * ProximalPose.R[:,tipSegmentIndex],
                                           ProximalPose.t + scale * ProximalPose.R[:,tipSegmentIndex]])
                 
-                DistalPose = self.distalPose()
+                DistalPose = self.DistalPose()
                 uhatDistal = DistalPose.R[:,uhatIndex]
                 vhatDistal = DistalPose.R[:,vhatIndex]
                 DistalBase = DistalPose.t + u.reshape(-1,1) @ uhatDistal.reshape(1,3) + v.reshape(-1,1) @ vhatDistal.reshape(1,3)

KinematicChain.py
@@ -14,8 +14,8 @@ from LinkCSC import LinkCSC
 A KinematicTree with no branching.
 """
 class KinematicChain(KinematicTree):
-    def __init__(self, root : Joint, maxAnglePerElbow : float = np.pi/2):
-        super().__init__(root, maxAnglePerElbow)
+    def __init__(self, startJoint : Joint, maxAnglePerElbow : float = np.pi/2):
+        super().__init__(startJoint, maxAnglePerElbow)
     
     """ Add the given joint to the end of the chain, return its index """
     def append(self, newJoint : Joint, relative : bool = True, 
@@ -31,8 +31,8 @@ class KinematicChain(KinematicTree):
         for j in range(1, len(self.Joints)):
             prevJoint = self.Joints[j-1]
             thisJoint = self.Joints[j]
-            link = LinkCSC(self.r, prevJoint.distalDubinsFrame(), 
-                                   thisJoint.proximalDubinsFrame(),
+            link = LinkCSC(self.r, prevJoint.DistalDubinsFrame(), 
+                                   thisJoint.ProximalDubinsFrame(),
                                    self.maxAnglePerElbow)
             linkPattern = link.creasePattern(numSides, twistPortion)
             pattern.append(linkPattern)

KinematicTree.py
@@ -32,8 +32,8 @@ class KinematicTree:
         self.numSides = root.numSides
         self.Joints = [root]
         self.Parents = [-1]     # root has no parent
-        self.Links = [LinkCSC(self.r, root.proximalDubinsFrame(),
-                                      root.proximalDubinsFrame(),
+        self.Links = [LinkCSC(self.r, root.ProximalDubinsFrame(),
+                                      root.ProximalDubinsFrame(),
                                       maxAnglePerElbow)]
         assert(maxAnglePerElbow >= 0 and maxAnglePerElbow <= np.pi)
         self.maxAnglePerElbow = maxAnglePerElbow 
@@ -96,8 +96,8 @@ class KinematicTree:
                                 undefined=parent.Pose.R[:,0])
             newJoint.setXhatAboutZhat(xhat)
         
-        newLink = LinkCSC(self.r, parent.distalDubinsFrame(), 
-                                  newJoint.proximalDubinsFrame(),
+        newLink = LinkCSC(self.r, parent.DistalDubinsFrame(), 
+                                  newJoint.ProximalDubinsFrame(),
                                   self.maxAnglePerElbow)
         
         self.boundingBall = minBoundingBall(self.boundingBall, 
@@ -134,8 +134,8 @@ class KinematicTree:
                   showJointSurface=True, jointColor=jointColorDefault,
                   jointAxisScale=jointAxisScaleDefault, showJointPoses=True,
                   linkColor=linkColorDefault, surfaceOpacity=surfaceOpacityDefault, showLinkSurface=True, 
-                  showLinkPoses=False, showLinkPath=True, 
-                  showPathCircles=False, sphereColor='black',
+                  showLinkPoses=False, showLinkPath=True, pathColor=pathColorDefault,
+                  showPathCircles=False, sphereColor=sphereColorDefault,
                   showSpheres=True):
         jointPlotHandles = []
         linkPlotHandles = []
@@ -153,6 +153,7 @@ class KinematicTree:
             handles = link.addToPlot(ax, color=linkColor, 
                                    alpha=surfaceOpacity, 
                                    showPath=showLinkPath, 
+                                   pathColor=pathColor,
                                    showPathCircles=showPathCircles, 
                                    showFrames=showLinkPoses,
                                    showBoundary=showLinkSurface)
@@ -172,8 +173,8 @@ class KinematicTree:
              showJointSurface=True, jointColor=jointColorDefault, 
              jointAxisScale=jointAxisScaleDefault, showJointPoses=True,
              linkColor=linkColorDefault, surfaceOpacity=surfaceOpacityDefault, showLinkSurface=True, 
-             showLinkPoses=False, showLinkPath=True, 
-             showPathCircles=False, sphereColor='black',
+             showLinkPoses=False, showLinkPath=True, pathColor=pathColorDefault,
+             showPathCircles=False, sphereColor=sphereColorDefault,
              showSpheres=False, block=blockDefault):
         ax = plt.figure().add_subplot(projection='3d')
         jointPlotHandles, linkPlotHandles = self.addToPlot(ax, 
@@ -182,7 +183,7 @@ class KinematicTree:
                                     showJointSurface, jointColor, 
                                     jointAxisScale, showJointPoses,
                                     linkColor, surfaceOpacity, showLinkSurface, 
-                                    showLinkPoses, showLinkPath, 
+                                    showLinkPoses, showLinkPath, pathColor,
                                     showPathCircles, sphereColor,
                                     showSpheres)
         
@@ -216,8 +217,8 @@ class KinematicTree:
         joint = self.Joints[jointIndex]
         if recomputeLinkPath and jointIndex > 0:
             parent = self.Joints[self.Parents[jointIndex]]
-            self.Links[jointIndex] = LinkCSC(self.r, parent.distalDubinsFrame(), 
-                                      joint.proximalDubinsFrame(),
+            self.Links[jointIndex] = LinkCSC(self.r, parent.DistalDubinsFrame(), 
+                                      joint.ProximalDubinsFrame(),
                                       self.maxAnglePerElbow)
         else:
             self.Links[jointIndex] = self.Links[jointIndex].newLinkTransformedBy(Transformation)
@@ -229,23 +230,31 @@ class KinematicTree:
         else:
             for c in self.Children[jointIndex]:
                 child = self.Joints[c]
-                self.Links[c] = LinkCSC(self.r, joint.distalDubinsFrame(), 
+                self.Links[c] = LinkCSC(self.r, joint.DistalDubinsFrame(), 
                                           child.proximalDubinsFrame(),
                                           self.maxAnglePerElbow)
         if recomputeBoundingBall:
             self.recomputeBoundingBall()
                 
     def setJointState(self, jointIndex : int, state : float):
-        Transformation = self.Joints[jointIndex].transformStateTo(state)
+        Transformation = self.Joints[jointIndex].TransformStateTo(state)
         for c in self.Children[jointIndex]:
             self.transformJoint(c, Transformation, recursive=True, 
                                 recomputeBoundingBall=False)
         self.recomputeBoundingBall()
         
     def translateJointAlongAxis(self, jointIndex : int, distance : float, 
-                                propogate : bool = True):
+                                propogate : bool = True, 
+                                applyToPreviousWaypoint : bool = False):
         Translation = SE3(distance * self.Joints[jointIndex].Pose.R[:,2])
-        self.transformJoint(jointIndex, Translation, propogate)
+        if applyToPreviousWaypoint and type(self.Joints[jointIndex-1])==Waypoint:
+            if propogate:
+                self.transformJoint(jointIndex-1, Translation, True)
+            else:
+                self.transformJoint(jointIndex-1, Translation, False)
+                self.transformJoint(jointIndex, Translation, False)
+        else:
+            self.transformJoint(jointIndex, Translation, propogate)
     
     def rotateJointAboutAxis(self, jointIndex : int, angle : float,
                              propogate : bool = True):

geometryHelpers.py
@@ -130,8 +130,11 @@ def signedAngles3D(A, B, n):
     return np.array([signedAngle(A[i], B[i], n) for i in range(k)]).flatten()
 
 # wrap angles to [0,2pi)
-def wrapAngle(angle):
-    return angle % (2*np.pi)
+def wrapAngle(angle, EPSILON=0.00001):
+    modResult = angle % (2*np.pi)
+    if abs(modResult-2*np.pi) < EPSILON:
+        return 0
+    return modResult
 
 class Circle3D:
     def __init__(self, radius, center, normal):

testJoint.py
@@ -12,7 +12,7 @@ r = 1
 testRevolute = RevoluteJoint(numSides, r, totalBendingAngle=np.pi, Pose=SE3.Rx(np.pi/3))
 #testRevolute.plot(showSphere = True)
 testRevolute.transformPoseBy(SE3(1,1,1) @ SE3.Ry(np.pi/4))
-testRevolute.transformStateTo(-np.pi/3)
+testRevolute.TransformStateTo(-np.pi/3)
 testRevolute.show(showSphere = True, block=False)
 
 testPrismatic = PrismaticJoint(numSides, r, neutralLength=4, numLayers=4, 
@@ -20,7 +20,7 @@ testPrismatic = PrismaticJoint(numSides, r, neutralLength=4, numLayers=4,
 #testPrismatic.plot(showSphere=True)
 testPrismatic.transformPoseBy(SE3(-2,3,5) @ SE3.Ry(np.pi/4))
 testPrismatic.show(showSphere=True, block=False)
-testPrismatic.transformStateTo(-1)
+testPrismatic.TransformStateTo(-1)
 testPrismatic.show(showSphere=True, block=False)
-testPrismatic.transformStateTo(0.6)
+testPrismatic.TransformStateTo(0.6)
 testPrismatic.show(showSphere=True, block=True)

testKinematicChain.py
@@ -11,17 +11,23 @@ numSides = 6
 # chain whose root is a waypoint at the global origin
 chain = KinematicChain(StartFingertip(numSides, r, Pose=SE3(), length=0.5)) 
 
-prismaticIndex = chain.append(PrismaticJoint(numSides, r, neutralLength=3, numLayers=6, 
-                        coneAngle=np.pi/4, Pose= SE3.Ry(np.pi/4) @ SE3.Trans([1,-3,0]) ) )
+chain.show(block=True, showJointSurface=False, showLinkSurface=False, showSpheres=True)
 
+prismaticIndex = chain.append(PrismaticJoint(numSides, r, neutralLength=3, numLayers=6, 
+                        coneAngle=np.pi/4, Pose= SE3.Trans([5,5,0]) ) )
+
+chain.show(block=True, showJointSurface=False, showLinkSurface=False, showSpheres=True)
 
 revoluteIndex = chain.append(RevoluteJoint(numSides, r, np.pi, 
-                                           SE3.Ry(np.pi/4) @ SE3.Trans([3,1,0])))
+                                           SE3.Ry(np.pi/4)))
 
-chain.append(EndFingertip(numSides, r, Pose=SE3(4,0,0)@SE3.Ry(np.pi/2), length=0.5))
+chain.show(block=True, showJointSurface=False, showLinkSurface=False, showSpheres=True)
 
-chain.show(block=False, showLinkPath=False, showJointPoses=False, showLinkPoses=False)
-chain.show(block=True, showJointSurface=False, showLinkSurface=False)
+chain.append(EndFingertip(numSides, r, Pose=SE3.Ry(np.pi/2), length=0.5))
+
+chain.show(block=True, showJointSurface=False, showLinkSurface=False, showSpheres=True)
+
+chain.show(block=True, showLinkPath=False, showJointPoses=False, showLinkPoses=False)
 
 """
 KC.translateJointAlongAxis(prismaticIndex, -5)