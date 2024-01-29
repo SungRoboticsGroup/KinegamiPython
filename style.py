0ae9ece526e93777c65060fbc667d017ffcd9676
Daniel Feshbach <danielfeshbach@gmail.com>
2024-01-29 00:54:08 -0500

    Adding style.py file, which I've been using for a while so should have added a while ago

---
+++ style.py
@@ -0,0 +1,24 @@
+import ezdxf
+
+# Chosen to be mutually distinguishable in grayscale
+xColorDefault='orangered'
+yColorDefault='lightgreen'
+zColorDefault='darkblue'
+
+pathColorDefault='black'
+sphereColorDefault='black'
+
+linkColorDefault='black'
+surfaceOpacityDefault=0.5
+jointColorDefault='magenta'
+
+jointAxisScaleDefault=10
+
+blockDefault=True
+
+# in AutoCAD Color Index 
+# https://ezdxf.readthedocs.io/en/stable/concepts/aci.html#aci
+mountainColorDefault=174  #dark blue
+valleyColorDefault=1 #red
+boundaryColorDefault=2 #yellow
+cutColorDefault=101 #green 3
