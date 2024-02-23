d6ea07b2b3857ecaa9d234e9662f0c619da5d590
sgmq0 <fengray@seas.upenn.edu>
2024-02-23 17:11:53 -0500

    Update points2.py

--- points2.py
+++ points2.py
@@ -1,15 +1,15 @@
+"""
+@author: Raymond Feng
+"""
+
 import sys
 import numpy as np
 import pyqtgraph.opengl as gl
 from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QPushButton, QDockWidget, QComboBox, QDoubleSpinBox, QHBoxLayout, QLabel
 from PyQt5.QtGui import QColor
 from PyQt5.QtCore import Qt
-
-'''
-- import SE3
-- import numpy as np
-- SE3.Rx() to rotate, @ to apply, * to apply to numpy vectors
-'''
+from spatialmath import SE3
+import math
 
 class PointEditorWindow(QMainWindow):
     def __init__(self):
@@ -26,6 +26,7 @@ class PointEditorWindow(QMainWindow):
         self.current_point = 0
 
         self.points = []
+        self.poses = []
 
         self.add_point_button = QPushButton("Add Point")
         self.add_point_button.clicked.connect(self.add_point)
@@ -33,47 +34,53 @@ class PointEditorWindow(QMainWindow):
         self.select_point_options.activated.connect(self.index_changed)
         self.select_point_options.currentIndexChanged.connect(self.index_changed)
 
-        self.move_x_spinbox = QDoubleSpinBox()
-        self.move_x_spinbox.valueChanged.connect(self.move_point_x)
-        self.move_x_spinbox.setRange(-20, 20)
-        self.move_x_spinbox.setSingleStep(0.1)
-        self.move_x_label = QLabel("X: ")
+        self.move_x_pos = QPushButton("+X")
+        self.move_x_pos.clicked.connect(self.move_point_x_pos)
+        self.move_x_neg = QPushButton("-X")
+        self.move_x_neg.clicked.connect(self.move_point_x_neg)
 
-        self.move_y_spinbox = QDoubleSpinBox()
-        self.move_y_spinbox.valueChanged.connect(self.move_point_y)
-        self.move_y_spinbox.setRange(-20, 20)
-        self.move_y_spinbox.setSingleStep(0.1)
-        self.move_y_label = QLabel("Y: ")
+        self.move_y_pos = QPushButton("+Y")
+        self.move_y_pos.clicked.connect(self.move_point_y_pos)
+        self.move_y_neg = QPushButton("-Y")
+        self.move_y_neg.clicked.connect(self.move_point_y_neg)
 
-        self.move_z_spinbox = QDoubleSpinBox()
-        self.move_z_spinbox.valueChanged.connect(self.move_point_z)
-        self.move_z_spinbox.setRange(-20, 20)
-        self.move_z_spinbox.setSingleStep(0.1)
-        self.move_z_label = QLabel("Z: ")
+        self.move_z_pos = QPushButton("+Z")
+        self.move_z_pos.clicked.connect(self.move_point_z_pos)
+        self.move_z_neg = QPushButton("-Z")
+        self.move_z_neg.clicked.connect(self.move_point_z_neg)
 
-        self.rotate_button = QPushButton("Rotate")
-        self.rotate_button.clicked.connect(self.rotate_points)
+        self.rotate_button_x = QPushButton("Rotate X")
+        self.rotate_button_x.clicked.connect(self.rotate_point_x)
+        self.rotate_button_y = QPushButton("Rotate Y")
+        self.rotate_button_y.clicked.connect(self.rotate_point_y)
+        self.rotate_button_z = QPushButton("Rotate Z")
+        self.rotate_button_z.clicked.connect(self.rotate_point_z)
 
         button_layout = QVBoxLayout()
         button_layout.addWidget(self.select_point_options)
         button_layout.addWidget(self.add_point_button)
 
         x_layout = QHBoxLayout()
-        x_layout.addWidget(self.move_x_label)
-        x_layout.addWidget(self.move_x_spinbox) 
+        x_layout.addWidget(self.move_x_neg)
+        x_layout.addWidget(self.move_x_pos) 
 
         y_layout = QHBoxLayout()
-        y_layout.addWidget(self.move_y_label)
-        y_layout.addWidget(self.move_y_spinbox) 
+        y_layout.addWidget(self.move_y_neg)
+        y_layout.addWidget(self.move_y_pos) 
 
         z_layout = QHBoxLayout()
-        z_layout.addWidget(self.move_z_label)
-        z_layout.addWidget(self.move_z_spinbox) 
+        z_layout.addWidget(self.move_z_neg)
+        z_layout.addWidget(self.move_z_pos) 
+
+        rotate_layout = QHBoxLayout()
+        rotate_layout.addWidget(self.rotate_button_x)
+        rotate_layout.addWidget(self.rotate_button_y)
+        rotate_layout.addWidget(self.rotate_button_z)
 
         button_layout.addLayout(x_layout)
         button_layout.addLayout(y_layout)
         button_layout.addLayout(z_layout)
-        #button_layout.addWidget(self.rotate_button)
+        button_layout.addLayout(rotate_layout)
 
         button_widget = QWidget()
         button_widget.setLayout(button_layout)
@@ -82,30 +89,58 @@ class PointEditorWindow(QMainWindow):
         dock.setWidget(button_widget)
 
     def add_point(self):
-        point = gl.GLScatterPlotItem(pos=np.array([0,0,0]), color=(1, 1, 1, 1), size=10)
+
+        #initialize pose
+        pose = SE3()
+        point = gl.GLScatterPlotItem(pos=pose.t, color=(1, 1, 1, 1), size=10)
+
         self.points.append(point)
+        self.poses.append(pose)
+
         self.select_point_options.addItem(str(len(self.points)))
 
         self.update_plot()
 
-    def move_point_x(self, value):
-        self.points[self.current_point].pos[0] = value
+    def move_point_x_pos(self):
+        self.poses[self.current_point] = self.poses[self.current_point] * SE3.Tx(.2)
+        self.points[self.current_point].pos = self.poses[self.current_point].t 
         self.update_plot()
 
-    def move_point_y(self, value):
-        self.points[self.current_point].pos[1] = value
+    def move_point_x_neg(self):
+        self.poses[self.current_point] = self.poses[self.current_point] * SE3.Tx(-.2)
+        self.points[self.current_point].pos = self.poses[self.current_point].t 
         self.update_plot()
 
-    def move_point_z(self, value):
-        self.points[self.current_point].pos[2] = value
+    def move_point_y_pos(self):
+        self.poses[self.current_point] = self.poses[self.current_point] * SE3.Ty(.2)
+        self.points[self.current_point].pos = self.poses[self.current_point].t 
         self.update_plot()
 
-    def rotate_points(self):
-        angle = 10 if QApplication.keyboardModifiers() == Qt.ShiftModifier else 1
-        point = self.points[self.current_point]
-        rot = point.opts['rotate']
-        point.opts['rotate'] = (rot[0], rot[1] + angle, rot[2])
+    def move_point_y_neg(self):
+        self.poses[self.current_point] = self.poses[self.current_point] * SE3.Ty(-.2)
+        self.points[self.current_point].pos = self.poses[self.current_point].t 
+        self.update_plot()
 
+    def move_point_z_pos(self):
+        self.poses[self.current_point] = self.poses[self.current_point] * SE3.Tz(.2)
+        self.points[self.current_point].pos = self.poses[self.current_point].t 
+        self.update_plot()
+
+    def move_point_z_neg(self):
+        self.poses[self.current_point] = self.poses[self.current_point] * SE3.Tz(-.2)
+        self.points[self.current_point].pos = self.poses[self.current_point].t 
+        self.update_plot()
+
+    def rotate_point_x(self):
+        self.poses[self.current_point] = self.poses[self.current_point] * SE3.Rx(math.pi/12)
+        self.update_plot()
+
+    def rotate_point_y(self):
+        self.poses[self.current_point] = self.poses[self.current_point] * SE3.Ry(math.pi/12)
+        self.update_plot()
+
+    def rotate_point_z(self):
+        self.poses[self.current_point] = self.poses[self.current_point] * SE3.Rz(math.pi/12)
         self.update_plot()
 
     def index_changed(self, index):
@@ -121,10 +156,12 @@ class PointEditorWindow(QMainWindow):
         grid = gl.GLGridItem()
         self.plot_widget.addItem(grid)
 
+        index = 0
         for point in self.points:
             md = gl.MeshData.sphere(rows=10, cols=20, radius=0.9)
             center = point.pos
-
+            
+            #plot sphere
             m1 = gl.GLMeshItem(
                 meshdata=md,
                 smooth=True,
@@ -135,10 +172,37 @@ class PointEditorWindow(QMainWindow):
             m1.translate(*center)
             self.plot_widget.addItem(m1)
 
+            #plot point
             self.plot_widget.addItem(point)
-            self.plot_widget.addItem(gl.GLLinePlotItem(pos=np.array([point.pos, [point.pos[0]+1, point.pos[1], point.pos[2]]]), color=(1, 0, 0, 1)))
-            self.plot_widget.addItem(gl.GLLinePlotItem(pos=np.array([point.pos, [point.pos[0], point.pos[1]+1, point.pos[2]]]), color=(0, 1, 0, 1)))
-            self.plot_widget.addItem(gl.GLLinePlotItem(pos=np.array([point.pos, [point.pos[0], point.pos[1], point.pos[2]+1]]), color=(0, 0, 1, 1)))
+
+            # -------- plot all the poses --------
+            pose = self.poses[index]
+            center = np.append(point.pos, 1)
+
+            #endpoints
+            p_x = np.append(point.pos + [1,0,0], 1)
+            p_y = np.append(point.pos + [0,1,0], 1)
+            p_z = np.append(point.pos + [0,0,1], 1)
+
+            #apply transformation to a centered point
+            rotated_point_x = np.dot(pose, p_x - center)
+            rotated_point_y = np.dot(pose, p_y - center)
+            rotated_point_z = np.dot(pose, p_z - center)
+    
+            #translate back from center
+            rotated_point_x += center
+            rotated_point_y += center
+            rotated_point_z += center
+
+            xLine = gl.GLLinePlotItem(pos=np.array([point.pos, rotated_point_x[:3]]), color=(1, 0, 0, 1)) 
+            yLine = gl.GLLinePlotItem(pos=np.array([point.pos, rotated_point_y[:3]]), color=(0, 1, 0, 1)) 
+            zLine = gl.GLLinePlotItem(pos=np.array([point.pos, rotated_point_z[:3]]), color=(0, 0, 1, 1)) 
+
+            self.plot_widget.addItem(xLine)
+            self.plot_widget.addItem(yLine)
+            self.plot_widget.addItem(zLine)
+
+            index = index + 1
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
