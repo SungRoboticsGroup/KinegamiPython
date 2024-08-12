2d78298cf1f6e8c9c5848311e18877d322f6bdce
LilyS284 <lilyrsand@gmail.com>
2024-08-11 22:16:46 -0400

    draft of symmetric hand positioning and joint editing

--- examples/trees/symmetricHandPrinted.py
+++ examples/trees/symmetricHandPrinted.py
@@ -38,13 +38,13 @@ thumb1 = tree.addJoint(thumb0, PrintedOrthogonalRevoluteJoint(r, -np.pi/2, np.pi
               fixedPosition=True, fixedOrientation=True)
 
 thumbEnd = tree.addJoint(thumb1, PrintedTip(r, 
-            SE3.Trans(4*multiplier,0,0)@SE3.Ry(np.pi/2)@SE3.Rz(np.pi/2), screwRadius),
+            SE3.Trans(3*multiplier,0,0)@SE3.Ry(np.pi/2)@SE3.Rz(np.pi/2), screwRadius),
               relative=True, safe=False, 
               fixedPosition=True, fixedOrientation=True)
 
 
 pointer1 = tree.addJoint(0, PrintedOrthogonalRevoluteJoint(r, -np.pi/2, np.pi/2,
-            SE3.Trans(0,0,5*multiplier)@SE3.Ry(-np.pi/2)@SE3.Rx(np.pi), screwRadius),
+            SE3.Trans(0,0,4*multiplier)@SE3.Ry(-np.pi/2)@SE3.Rx(np.pi), screwRadius),
               relative=True, safe=False, 
               fixedPosition=True, fixedOrientation=True)
 
@@ -58,62 +58,62 @@ pointerEnd = tree.addJoint(pointer2, PrintedTip(r,
               relative=True, safe=False, 
               fixedPosition=True, fixedOrientation=True)
 
-middleJoint = PrintedOrthogonalRevoluteJoint(r, -np.pi/2, np.pi/2, SE3.Trans(-3*multiplier,0,1)@SE3.Ry(0)@SE3.Rx(0)@SE3.Rz(np.pi), screwRadius)
+middleJoint = PrintedOrthogonalRevoluteJoint(r, -np.pi/2, np.pi/2, SE3.Trans(-2*multiplier,0,1)@SE3.Ry(0)@SE3.Rx(-np.pi/2)@SE3.Rz(np.pi), screwRadius)
 middle0 = tree.addJoint(0, middleJoint,
               relative=True, safe=False, 
               fixedPosition=True, fixedOrientation=True)
 
-middle1Joint = PrintedOrthogonalRevoluteJoint(r, -np.pi/2, np.pi/2, SE3.Trans(5*multiplier,0,0)@SE3.Rx(np.pi/4), screwRadius)
+middle1Joint = PrintedOrthogonalRevoluteJoint(r, -np.pi/2, np.pi/2, SE3.Trans(3*multiplier,0,0)@SE3.Rx(np.pi/2), screwRadius)
 middle1 = tree.addJoint(middle0, middle1Joint,
               relative=True, safe=False, 
               fixedPosition=True, fixedOrientation=True)
 
 middle2 = tree.addJoint(middle1, PrintedOrthogonalRevoluteJoint(r, -np.pi/2, np.pi/2,
-                        SE3.Trans(4*multiplier,0,0), screwRadius),
+                        SE3.Trans(4*multiplier,0,0)@SE3.Rx(-np.pi/4), screwRadius),
               relative=True, safe=False, 
               fixedPosition=True, fixedOrientation=True)
 
 middleEnd = tree.addJoint(middle2, PrintedTip(r, 
-        SE3.Trans(4*multiplier,0,0)@SE3.Ry(np.pi/2)@SE3.Rz(np.pi/2), screwRadius),
+        SE3.Trans(3*multiplier,0,0)@SE3.Ry(np.pi/2)@SE3.Rz(np.pi/2), screwRadius),
               relative=True, safe=False, 
               fixedPosition=True, fixedOrientation=True)
 
 # # Spherical grasp
-# tree.setJointState(middle0, np.pi/5.5)
-# tree.setJointState(palm, -np.pi/5.5)
+# tree.setJointState(middle0, -np.pi/3)
+# tree.setJointState(palm, np.pi/3)
 # for i in [middle1, middle2, pointer1, pointer2, thumb0, thumb1]:
 #     tree.setJointState(i, 0)
-#     tree.setJointState(middle1, np.pi/3.25)
-#     tree.setJointState(middle2, np.pi/3.75)
-#     tree.setJointState(thumb0, -np.pi/3.25)
-#     tree.setJointState(thumb1, -np.pi/3.75)
+#     tree.setJointState(middle1, -np.pi/2)
+#     tree.setJointState(middle2, -np.pi/3.25)
+#     tree.setJointState(thumb0, -np.pi/2)
+#     tree.setJointState(thumb1, -np.pi/3.25)
 #     tree.setJointState(pointer1, np.pi/2)
-#     tree.setJointState(pointer2, np.pi/4.25)
+#     tree.setJointState(pointer2, np.pi/2.5)
 
 
 # # Cylindrical grasp
-# tree.setJointState(middle0, np.pi/2)
+# tree.setJointState(middle0, -np.pi/2)
 # tree.setJointState(palm, np.pi/2)
 # for i in [middle1, middle2, pointer1, pointer2, thumb0, thumb1]:
 #     tree.setJointState(i, 0)
-#     tree.setJointState(middle1, np.pi/4)
-#     tree.setJointState(middle2, np.pi/3.75)
+#     tree.setJointState(middle1, -np.pi/2)
+#     tree.setJointState(middle2, -np.pi/4)
 #     tree.setJointState(thumb0, -np.pi/2)
 #     tree.setJointState(thumb1, -np.pi/4)
 #     tree.setJointState(pointer1, np.pi/2)
-#     tree.setJointState(pointer2, np.pi/4)
+#     tree.setJointState(pointer2, np.pi/2.5)
 
 # # Pinch grasp
-# tree.setJointState(middle0, np.pi/8)
+# tree.setJointState(middle0, np.pi/2)
 # tree.setJointState(palm, -np.pi/2)
 # for i in [middle2, pointer2, thumb1]:
 #     tree.setJointState(i, 0)
-#     tree.setJointState(middle1, np.pi/2)
+#     tree.setJointState(middle1, -np.pi/2)
 #     tree.setJointState(middle2, 0)
 #     tree.setJointState(thumb0, -np.pi/2)
 #     tree.setJointState(thumb1, 0)
-#     tree.setJointState(pointer1, np.pi/2.75)
-#     tree.setJointState(pointer2, np.pi/4)
+#     tree.setJointState(pointer1, np.pi/3.25)
+#     tree.setJointState(pointer2, np.pi/3)
 
 
 plotPrintedTree(tree, "manualHandPrinted")
