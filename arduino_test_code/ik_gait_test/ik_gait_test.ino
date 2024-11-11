df7e19f5e3faa06e98ee0d33e0a6d85dfad5d089
EmilSchaumburg <emil.schaumburg@gmail.com>
2024-11-11 13:53:35 -0500

    phase gait

--- arduino_test_code/ik_gait_test/ik_gait_test.ino
+++ arduino_test_code/ik_gait_test/ik_gait_test.ino
@@ -1,17 +1,20 @@
 #include <Servo.h>
 
-const float frequency = 0.0025;
+const float frequency = 0.0025f;
 
 Servo servos[14];
 
 int servoPins[14] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
 
-float servoOffsets[14] = {0, 90, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+float servoOffsets[14] = {0, 90, 0, 90, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 float servoSigns[14] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
 
-float L1 = 8.5; //hipLength; //TODO: measure
-float L2 = 8.5;// kneeLength; // TODO: measure
-float radius = 4; //TODO: decide
+bool highFreq[6] = {true,true,true,true,true,true};
+int counts[6] = {0,0,0,0,0,0};
+
+float L1 = 8.5; //cm
+float L2 = 8.5;
+float radius = 4; 
 
 float timer;
 
@@ -30,20 +33,40 @@ void moveServos(float t) {
   for (int i = 0; i < 6; i++) {
     int hipIndex = i*2;
     int kneeIndex = i*2 + 1;
-    float x = radius*cos(frequency*t + i*PI/6);
-    float y = radius*sin(frequency*t + i*PI/6);
+    float x,y;
+
+    if (highFreq[i]) {
+      //semicircle part of motion
+      x = radius*cos(frequency*t + i*PI/6.f);
+      y = radius*sin(frequency*t + i*PI/6.f);
+    } else {
+      //dragging along the floor (five times as long)
+      x = radius*cos(frequency*t / 5.f + i*PI/30.f - 6*PI/5.f * counts[i]);
+      y = radius*sin(frequency*t / 5.f + i*PI/30.f - 6*PI/5.f * counts[i]);
+    }
+
+    //switch between phases of motion
+    if (highFreq[i] && (y < 0)) {
+      highFreq[i] = false;
+      counts[i] = (counts[i] + 1) % 6;
+      x = radius*cos(frequency*t / 5.f + i*PI/30.f - 6*PI/5.f * counts[i]);
+      y = radius*sin(frequency*t / 5.f + i*PI/30.f - 6*PI/5.f * counts[i]);
+    } else if (!highFreq[i] && (y > 0)) {
+      highFreq[i] = true;
+      x = radius*cos(frequency*t + i*PI/6);
+      y = radius*sin(frequency*t + i*PI/6);
+    }
+
     if (y < 0) {
       y = 0;
     }
 
-    Serial.println(x, y);
-    y = L1 + L2 - y - 1; //subtract one for tolerance
+    // Serial.println(x, y);
+    y = L1 + L2 - y - 1; //subtract one for tolerance, so it can drag on the floor
     
-    // x = 0.41289672736166194;
-    // y = 11.017077534966651;
 
-    Serial.println(theta1(x,y));
-    Serial.println(theta2(x,y));
+    // Serial.println(theta1(x,y));
+    // Serial.println(theta2(x,y));
 
     float angle1 = theta1(x,y);
     if (x < 0) {
@@ -56,7 +79,6 @@ void moveServos(float t) {
 }
 void setup() {
   Serial.begin(9600);
-  Serial.println("HEY");
 
   for (int i = 0; i < 14; i++) {
     servos[i].attach(servoPins[i]);
