f1c2a24c420796b6ecaaaf7eb80b0ab6bd71bafd
Raymond Feng <fengray@seas.upenn.edu>
2025-05-12 20:39:45 -0400

    make redos work

--- GUI.py
+++ GUI.py
@@ -1261,7 +1261,7 @@ class PointEditorWindow(QMainWindow):
         self.units = key
         self.units_label.setText(f"Current units: {self.units}")
         self.chain.units = key
-        self.log_version()
+        # self.log_version()
         self.update_joint()
 
     def rescale_dimensions(self, prev, new):
@@ -1298,6 +1298,7 @@ class PointEditorWindow(QMainWindow):
 
             self.plot_widget.opts['distance'] *= factor
 
+            self.log_version()
             self.update_joint()
 
     def edit_dims_func(self):
@@ -1312,6 +1313,7 @@ class PointEditorWindow(QMainWindow):
         self.grid.setSpacing(self.grid_spacing, self.grid_spacing, self.grid_spacing)
         
         # print(self.grid_size, self.grid_spacing)
+        self.log_version()
         self.update_joint()
         # self.grid.setSpacing(self.grid_spacing)
 
@@ -1353,15 +1355,26 @@ class PointEditorWindow(QMainWindow):
         print("LOG     version: " + str(self.total_version_counter) + ", size: " + str(len(self.versions)) + ", index: " + str(self.version_index))
 
     def undo(self):
+        prev_units = self.chain.units if self.chain else None
+
         if self.version_index > 0:
             self.version_index -= 1
+
             self.chain = copy.deepcopy(self.versions[self.version_index])
+            new_units = self.chain.units if self.chain else None
             self.units = self.chain.units
         else:
             if self.version_index == 0:
                 self.version_index = -1
+            
+            new_units = prev_units
             self.chain = None
         # self.reload_IDs()
+
+        # handle unit change
+        if prev_units != new_units:
+            self.rescale_dimensions(prev_units, new_units)
+
         self.update_joint()
 
         print("UNDO    version: " + str(self.total_version_counter) + ", size: " + str(len(self.versions)) + ", index: " + str(self.version_index))
@@ -1369,11 +1382,20 @@ class PointEditorWindow(QMainWindow):
     def redo(self):
         if self.version_index + 1 < len(self.versions):
             self.version_index += 1
+            prev_units = self.chain.units if self.chain else None
+            
             self.chain = copy.deepcopy(self.versions[self.version_index])
+            new_units = self.chain.units if self.chain else None
+
+            # handle unit change
+            if prev_units != new_units:
+                self.rescale_dimensions(prev_units, new_units)
+                self.change_units(new_units)
+
             self.update_joint()
 
         print("REDO    version: " + str(self.total_version_counter) + ", size: " + str(len(self.versions)) + ", index: " + str(self.version_index))
-    
+
     def toggle_grid_func(self):
         if self.grid_on:
             self.plot_widget.removeItem(self.grid)
@@ -2114,6 +2136,8 @@ class PointEditorWindow(QMainWindow):
         else:
             self.rotation_slider.setDisabled(True)
             self.translate_slider.setDisabled(True)
+
+        #print("current radius: " + str(self.chain.r))
                 
     def create_axis_label(self, text, color):
         line_pixmap = QPixmap(20, 2)
